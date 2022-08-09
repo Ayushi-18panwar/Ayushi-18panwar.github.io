5c45506b51ac00fd96553582692bc2d0383acabc
Ayushi-18panwar <86158532+Ayushi-18panwar@users.noreply.github.com>
2022-08-09 12:17:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed= new Typed(".typing",{
+    strings: ["Web Developer","Trainee at AccioJob", "Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+var typed= new Typed(".typing-2",{
+    strings: ["Coder","Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+
+
